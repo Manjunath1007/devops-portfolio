e89d0661b08dd57aeaf9ea555cfab6ea90894915
manjunath <manjunath.rathod@gmail.com>
2026-05-31 10:30:29 +0000

    feat: deploy microservices using docker swarm with replicas, HA, and failover

--- docker-production-assignment/services/transaction-service/App.java
+++ docker-production-assignment/services/transaction-service/App.java
@@ -1,5 +1,10 @@
+
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Transaction Service Running");
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            System.out.println("Transaction Service Running...");
+            Thread.sleep(5000);
+        }
     }
 }
